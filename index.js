615ab6b8378aec070b77c3095d69aa1fcaf4bcd8
show3567 <ydong011@fiu.edu>
2022-01-29 14:58:02 -0600

    update

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const morgan = require('moment');
 const app = express();
 
-require('./startup/logging')();
-require('./startup/db')();
-require('./startup/config')();
-require('./startup/validation')();
-require('./startup/routes')(app);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
 
-const port = process.env.PORT || 4231;
-const server = app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+
+// in the terminal: export NODE_ENV=development
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+app.get('/api/courses/:year/:month', (req, res) => {
+    res.send(req.params);
 });
 
-module.exports = server;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+});
