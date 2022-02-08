5822c3ead23589f040a1d3388f6a5b60011d48c5
Yusheng <yusheng.zhang@antra.com>
2022-02-07 19:37:15 -0500

    add get user profile api

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt'); // hush password
+const Joi = require("@hapi/joi");
+
+const { UserProfile } = require('../models/userProfile');
+
+router.get("/getProfile/:userEmail", async (req, res) => {  
+    const user = await UserProfile.findOne({ userEmail: req.params.userEmail });
+    if (!user) {
+        return res.status(404).send("user not found.");
+    }
+    res.send(user);
+});
+
+
+module.exports = router;
