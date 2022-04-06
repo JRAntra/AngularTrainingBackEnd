dbb017de17634b909caf0617a59225c1fbbda266
show3567 <ydong011@fiu.edu>
2022-04-05 19:47:15 -0500

    update

--- routes/news.js
+++ routes/news.js
@@ -7,24 +7,24 @@ const auth = require("../middleware/auth");
 const { News, validate } = require("../models/news");
 
 // router.get('/', auth, async (req, res) => {
-router.get("/",       async (req, res) => {
-  const news = await News.find().sort({ id: 1 });
-  res.send(news);
+router.get("/", async (req, res) => {
+    const news = await News.find().sort({ id: 1 });
+    res.send(news);
 });
 
-router.get('/:id', async (req, res) => {
-    const news = await News.findOne({id: req.params.id});
+router.get("/:id", async (req, res) => {
+    const news = await News.findOne({ id: req.params.id });
     res.send(news);
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const news = new News(req.body);
+    const news = new News(req.body);
 
-  await news.save();
-  res.send(news);
+    await news.save();
+    res.send(news);
 });
 
 // router.post('/:id', async (req, res) => {
@@ -39,61 +39,64 @@ router.post("/", async (req, res) => {
 // })
 
 router.patch("/addComment/:id", async (req, res) => {
-  const news = await News.find({ _id: req.params.id });
-  if (!news.length) {
-      return res.status(404).send(JSON.stringify("Story not found."));
-  }
+    const news = await News.find({ _id: req.params.id });
+    if (!news.length) {
+        return res.status(404).send(JSON.stringify("Story not found."));
+    }
 
-  const query = { _id: req.params.id };
-  const update = {
-    $push: {
-      comment: {
-        publisherName: req.body.publisherName,
-        publishedTime: req.body.publishedTime,
-        content: {
-          image: req.body.content.image,
-          video: req.body.content.video,
-          text: req.body.content.text,
+    const query = { _id: req.params.id };
+    const update = {
+        $push: {
+            comment: {
+                publisherName: req.body.publisherName,
+                content: {
+                    image: req.body.content.image,
+                    video: req.body.content.video,
+                    text: req.body.content.text,
+                },
+            },
         },
-      },
-    },
-  };
-  const options = { upsert: false };
-  await News.updateOne(query, update, options)
-    .then((result) => {
-      const { matchedCount, modifiedCount } = result;
-      if (matchedCount && modifiedCount) {
-        console.log(`Successfully added a new comment.`);
-      }
-    })
-    .catch((err) => console.error(`Failed to add comment: ${err}`));
+    };
+    const options = { upsert: false };
+    await News.updateOne(query, update, options)
+        .then((result) => {
+            // console.log(result);
+            const { matchedCount, modifiedCount } = result;
+            if (matchedCount && modifiedCount) {
+                console.log(`Successfully added a new comment.`);
+            }
+        })
+        .catch((err) => console.error(`Failed to add comment: ${err}`));
 
-  res.send(await News.find(query));
+    const newsupdated = (await News.find(query))[0];
+    const comment = newsupdated.comment[newsupdated.comment.length - 1];
+
+    res.send(comment);
 });
 
 router.delete("/deletePost/:id", async (req, res) => {
-  const news = await News.find({ _id: req.params.id });
+    const news = await News.find({ _id: req.params.id });
 
-  if (!news.length)
-    return res.status(404).send(JSON.stringify("Post is not exist."));
+    if (!news.length)
+        return res.status(404).send(JSON.stringify("Post is not exist."));
 
-  await News.deleteOne({ _id: req.params.id });
-  res.send(JSON.stringify("Post has been deleted."));
+    await News.deleteOne({ _id: req.params.id });
+    res.send(JSON.stringify("Post has been deleted."));
 });
 
-router.delete('/deleteComment/:postId/:commentId', async (req, res) => {
-  try {
-    let result = await News.updateOne(
-      { _id: req.params.postId },
-      {
-        $pull: { "comment": { _id: req.params.commentId } }
-      }
-    );
-    res.send(result);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
-  }
+router.delete("/deleteComment/:postId/:commentId", async (req, res) => {
+    try {
+        let result = await News.updateOne(
+            { _id: req.params.postId },
+            {
+                $pull: { comment: { _id: req.params.commentId } },
+            }
+        );
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong");
+    }
 });
 
 // const validateQuestion = question => {
